4c343d36ab00612e9d353896faa99711d5368f5a
inghamn <inghamn@bloomington.in.gov>
2017-06-28 10:53:06 -0400

    Switched to GPL2 license
    Fixes #3

--- src/Plugin/Block/EventsBlock.php
+++ src/Plugin/Block/EventsBlock.php
@@ -1,7 +1,22 @@
 <?php
 /**
  * @copyright 2017 City of Bloomington, Indiana
- * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @license https://www.gnu.org/licenses/old-licenses/gpl-2.0 GNU/GPL2, see LICENSE
+ *
+ * This file is part of the Google Calendar drupal module.
+ *
+ * The calendar module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The calendar module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the calendar module.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0/>.
  */
 namespace Drupal\calendar\Plugin\Block;
 
