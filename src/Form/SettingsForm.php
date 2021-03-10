d3137a8d0eade25887f90a49c4ed5e3325c8036d
Cliff Ingham <inghamn@bloomington.in.gov>
2021-03-10 13:19:05 -0500

    Added calendar link to breadcrumbs
    Updates #14

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -1,22 +1,7 @@
 <?php
 /**
- * @copyright 2017 City of Bloomington, Indiana
+ * @copyright 2017-2021 City of Bloomington, Indiana
  * @license https://www.gnu.org/licenses/old-licenses/gpl-2.0 GNU/GPL2, see LICENSE
- *
- * This file is part of the Google Calendar drupal module.
- *
- * The calendar module is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * The calendar module is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with the calendar module.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0/>.
  */
 namespace Drupal\calendar\Form;
 
@@ -25,25 +10,16 @@ use Drupal\Core\Form\FormStateInterface;
 
 class SettingsForm extends ConfigFormBase
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getFormId()
     {
         return 'calendar_settings';
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getEditableConfigNames()
     {
         return ['calendar.settings'];
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function buildForm(array $form, FormStateInterface $form_state)
     {
         $config = $this->config('calendar.settings');
@@ -53,17 +29,21 @@ class SettingsForm extends ConfigFormBase
             '#title'         => 'Google User Email',
             '#default_value' => $config->get('google_user_email')
         ];
+        $form['calendar_breadcrumb'] = [
+            '#type'          => 'textfield',
+            '#title'         => 'Breadcrumbs',
+            '#default_value' => $config->get('calendar_breadcrumb'),
+            '#description'   => 'Enter node IDs, separated by commas, to be used as the base breadcrumb for calendar routes'
+        ];
 
         return parent::buildForm($form, $form_state);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         $this->config('calendar.settings')
-             ->set('google_user_email', $form_state->getValue('google_user_email'))
+             ->set('google_user_email',   $form_state->getValue('google_user_email'))
+             ->set('calendar_breadcrumb', $form_state->getValue('calendar_breadcrumb'))
              ->save();
 
         parent::submitForm($form, $form_state);
--- src/GoogleGateway.php
+++ src/GoogleGateway.php
@@ -1,22 +1,7 @@
 <?php
 /**
- * @copyright 2017-2018 City of Bloomington, Indiana
+ * @copyright 2017-2021 City of Bloomington, Indiana
  * @license https://www.gnu.org/licenses/old-licenses/gpl-2.0 GNU/GPL2, see LICENSE
- *
- * This file is part of the Google Calendar drupal module.
- *
- * The calendar module is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * The calendar module is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with the calendar module.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0/>.
  */
 namespace Drupal\calendar;
 
@@ -41,8 +26,18 @@ class GoogleGateway
     }
 
     /**
+     * @see https://developers.google.com/calendar/v3/reference/calendars/get
+     * @throws Google_Service_Exception
+     */
+    public static function calendar(string $calendar_id): \Google_Service_Calendar_Calendar
+    {
+        $service = new \Google_Service_Calendar(self::getClient());
+        return $service->calendars->get($calendar_id);
+    }
+
+    /**
      * @see https://developers.google.com/calendar/v3/reference/events/get
-     * @throws \Google_Service_Exception
+     * @throws Google_Service_Exception
      */
     public static function event(string $calendar_id, string $event_id): \Google_Service_Calendar_Event
     {
